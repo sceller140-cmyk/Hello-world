76553f4c669cfaba5c4c03adc8d6561780d2fde4
sceller140-cmyk <sceller140@gmail.com>
2026-09-09 00:41:07 +0100

    Add files via upload

---
+++ chapitre-01/double-doubler_un_entier/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    int nombre;
+
+    // Lecture de l'entier sur l'entrée standard
+    std::cin >> nombre;
+
+    // Calcul et affichage du double
+    std::cout << nombre * 2 << std::endl;
+
+    return 0;
+}
